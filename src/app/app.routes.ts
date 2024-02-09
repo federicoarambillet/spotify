308cbda93971baeff57c7f2f0d02f4c8dc2e1bed
federicoarambillet <federicoarambillet37@gmail.com>
2024-02-09 18:47:21 -0300

    Basic structure of the project and paths.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '', // Localhost
+        // component:
+    },
+    {
+        path: 'Home',
+        // component:
+    }
+];
