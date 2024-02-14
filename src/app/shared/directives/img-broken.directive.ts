0fc6c4e2e886ba389d310c41947f9295450b922e
federicoarambillet <federicoarambillet37@gmail.com>
2024-02-14 16:57:02 -0300

    Add directives img

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appImgBroken]'
+  //Only apply to images therefore IMG.
+  selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
+  @HostListener('error') handleError(): void {
+    const elNative = this.elHost.nativeElement;
+    elNative.src = "../../../assets/images/img-broken.png"
+  }
 
-  constructor() { }
+  constructor(private elHost: ElementRef) { }
 
 }
